09e7a0f8bb328a488b7e93f25409e9a3a8eaa3ea
naveen pandey <55925530+naveenpandey98@users.noreply.github.com>
2022-07-20 12:01:01 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,8 @@
 
   $(window).on('load', function() { // makes sure the whole site is loaded 
   $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(150).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(150).css({'overflow':'visible'});
+  $('#preloader').delay(50).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(50).css({'overflow':'visible'});
 })
 
   // Back to top button
